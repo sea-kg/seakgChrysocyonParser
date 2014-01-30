df8c160b75b6831074756bc396c64e4447a105e1
sea-kg <mrseakg@gamail.com>
2014-01-31 02:34:38 +0700

    added usage

--- scansrc/src/main.cpp
+++ scansrc/src/main.cpp
@@ -1,3 +1,10 @@
-int main(int argc, char *argv[]) {
+#include <iostream>
+#include "../seakgChrysocyonParser/seakgChrysocyonParser.h"
 
+
+
+int main(int argc, char *argv[]) {
+	if(argc != 4) {
+		std::cout << argv[0] << " <folder_with_sources> <project_name> <folder_for_output_xml> \n\n";
+	};
 };
